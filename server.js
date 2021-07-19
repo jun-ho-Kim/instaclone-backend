6ba9e1083e396f3956cedf7cb2e87c68397389c9
todayIsHoho <wnsgh5049@naver.com>
2021-07-19 19:48:10 +0900

    POC API

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 import { ApolloServer, gql } from 'apollo-server'
 
 const typeDefs = gql`
+    type Movie {
+        title: String
+        description: String
+        year: Int
+    }
     type Query {
-        hello: String
+        movies: [Movie]
+        movie: Movie
+    }
+    type Mutation {
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        hello:() => "hello",
-    }
+        movies:() => [],
+        movie:() => ({title: "hi"})
+    },
+    Mutation: {
+        createMovie: (_, {title}) => {
+            console.log(title);
+            return true;
+        },
+        deleteMovie: (_, {title}) => {
+            console.log(title)
+            return true;
+        }
+    },
 };
 
 const server = new ApolloServer({
