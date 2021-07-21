b3028dc6f2acb5c7cdec61952af352ef9ab65add
todayIsHoho <wnsgh5049@naver.com>
2021-07-22 07:33:15 +0900

    Architecture_Merge, makeExecutalbesSchema

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import { ApolloServer, gql } from 'apollo-server';
-import client from './client';
+import { ApolloServer } from 'apollo-server';
+import schema from './schema'
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema
 })
 
 server.listen().then(()=> console.log("Server is running on http://localhost:4000/"));
