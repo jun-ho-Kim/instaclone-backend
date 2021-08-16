28455a7e7b0bca7e028a4d3e89250e63cecaa76a
todayIsHoho <wnsgh5049@naver.com>
2021-08-16 15:33:47 +0900

    refactor: protectResolver(Query를 실행할 경우 return 값 null로 반환)

---
+++ src/users/users.utils.ts
@@ -0,0 +1,38 @@
+import * as jwt from 'jsonwebtoken';
+import client from "../client";
+import { Resolver } from '../types';
+
+export const getUser = async (token) => {
+    try {
+        if (!token) {
+            return null;
+        }
+        const { id }: any = await jwt.verify(token, process.env.SECRET_KEY);
+        const IntId = id;
+        const user = await client.user.findUnique({ where: { id: IntId } });
+        if (user) {
+            return user;
+        } else {
+            return null;
+        }
+    } catch {
+        return null;
+    }
+};
+
+export const protectResolver = (ourResolver: Resolver) => (root, args, context, info) => {
+    const query = info.operation.operation
+
+    if (!context.loggedInUser) {
+        if (query) {
+            return null
+        } else {
+            return {
+                ok: false,
+                error: "유저를 찾을 수 없습니다."
+            }
+        }
+    };
+
+    return ourResolver(root, args, context, info)
+}
